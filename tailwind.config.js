bf38916263b2696db4248756de4598485f480248
Matt Heslington <matt.heslington@icloud.com>
2020-09-24 15:22:23 +0800

    🚀 Lift-off

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    purge: [
+        'src/**/*.js',
+        'src/**/*.jsx',
+        'src/**/*.ts',
+        'src/**/*.tsx',
+        'public/**/*.html',
+    ],
+    theme: {
+        extend: {
+            colors: {
+            "bg": "#242526",
+            "bg-accent": "#484A4D",
+            "text-colour": "#DADCE1",
+            "border": "#474A4D",
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+}
